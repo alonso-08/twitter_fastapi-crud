7faffb9f9a6a9bcae44041e0de3948b47043fde3
Luis Alonso <luis.alonsoll89@gmail.com>
2022-03-25 22:58:09 -0600

    path_operations_structure_users

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Python
 from datetime import date, datetime
-from typing import Optional
+from turtle import title
+from typing import Optional, List
 from uuid import UUID
 import uuid
 
@@ -11,7 +12,7 @@ from pydantic import Field
 
 #FastApi
 from fastapi import FastAPI
-
+from fastapi import status
 app=FastAPI()
 
 
@@ -52,6 +53,79 @@ class Tweet():
     update_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
+
+# Path Operations
+
 @app.get("/")
 def home():
     return {"mensaje":"Funcionando"}
+
+## Users
+
+@app.post(
+    path="/signup/"
+    , response_model=User
+    , status_code=status.HTTP_201_CREATED
+    , summary="Register a User"
+    , tags=["Users"]
+
+)
+def signup():
+    pass
+
+@app.post(
+    path="/login/"
+    , response_model=User
+    , status_code=status.HTTP_200_OK
+    , summary="Login a User"
+    , tags=["Users"]
+
+)
+def login():
+    pass
+
+@app.get(
+    path="/users/"
+    , response_model=List[User]
+    , status_code=status.HTTP_201_CREATED
+    , summary="Show all Users"
+    , tags=["Users"]
+
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}"
+    , response_model=User
+    , status_code=status.HTTP_200_OK
+    , summary="Show a User"
+    , tags=["Users"]
+
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}/delete"
+    , response_model=User
+    , status_code=status.HTTP_200_OK
+    , summary="Delete a User"
+    , tags=["Users"]
+
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/users/{user_id}/update"
+    , response_model=User
+    , status_code=status.HTTP_200_OK
+    , summary="Update a User"
+    , tags=["Users"]
+
+)
+def update_a_user():
+    pass
+## Tweets
+
